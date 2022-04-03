ff8d46bb1d38366aac50afad389bc75224ef80b0
wafaaezzat <wafaaezzat45@icloud.com>
2022-04-03 21:24:27 +0200

    final touch

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,67 @@
+<template>
+   <base-card>
+      <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButton">Stored Resources </base-button>
+      <base-button @click="setSelectedTab('add-resource')" :mode="addResButton">Add Resource </base-button>
+   </base-card>
+   <keep-alive>
+   <component :is="selectedTab"></component>
+   </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue'
+import AddResource from './AddResource.vue'
+export default {
+    components:{StoredResources,AddResource},
+   data(){
+       return{
+selectedTab:'stored-resources',
+         
+storedResources:[
+    {id:'official-guide',
+    title:'Official Guide',
+    description:'the official vuejs documentation.'
+    ,link:'https://vuejs.org'},
+    {id:'google'
+    ,title:'Google'
+    ,description:'learn to google...'
+    ,link:'https://google.com'},
+    ]
+       };
+   },
+   provide(){
+       return{
+         resources:this.storedResources,
+         addResource:this.addResource,
+         deleteResource:this.removeResource,
+       };
+   },
+   methods:{
+       setSelectedTab(tab){
+        this.selectedTab=tab;
+       },
+       addResource(title,description,url){
+           const newResource={
+               id:new Date().toISOString(),
+               title:title,
+               description:description,
+               link:url
+           }
+           this.storedResources.unshift(newResource);
+           this.selectedTab='stored-resources'
+       },
+       removeResource(resId){
+        const resIndex=this.storedResources.findIndex((res)=>res.id===resId);
+        this.storedResources.splice(resIndex,1);
+       }
+   },
+   computed:{
+       storedResButton(){
+           return this.selectedTab==='stored-resources'? null:'flat';
+       },
+       addResButton(){
+           return this.selectedTab==='add-resource'? null:'flat';
+       }
+   }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BaseCard from './components/UI/BaseCard.vue'
+import BaseButton  from './components/UI/BaseButton.vue'
+import BaseDialog from './components/UI/BaseDialog.vue'
+const app=createApp(App);
+app.component('base-card',BaseCard);
+app.component('base-button',BaseButton);
+app.component('base-dialog',BaseDialog)
 
-createApp(App).mount('#app')
+app.mount('#app');
